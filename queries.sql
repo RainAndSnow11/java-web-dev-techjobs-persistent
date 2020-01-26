9ac0c2271d135647cac67c384ea2c045e13e7f10
name <email@email.com>
2020-01-25 20:30:53 -0600

    finished Part 1

--- queries.sql
+++ queries.sql
@@ -1,5 +1,10 @@
 ## Part 1: Test it with SQL
 
+--Columns: id(int), employer(varchar), name(varchar), skills(varchar)
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'job';
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
