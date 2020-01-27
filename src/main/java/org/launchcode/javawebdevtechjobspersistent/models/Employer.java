a65f4ff1c66c8722bdde58a6d297ffbe3e16a3c3
name <email@email.com>
2020-01-26 20:11:15 -0600

    Finished up to Data Layer in part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="Location is required.")
+    @Size(min=2, max=100, message="Location must be between 2 and 100 characters long.")
+    private String location;
 
+    public Employer(){
+    }
+
+    public Employer(String aLocation){
+        super();
+        this.location = aLocation;
+    }
+
+
+    // Getter and setter
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max=500, message="Description must be no longer than 500 characters.")
+    private String description;
+
+    public Skill(){
+    }
+
+    public Skill(String aDescription){
+        super();
+        this.description = aDescription;
+    }
+
+    // Getter and setter
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
