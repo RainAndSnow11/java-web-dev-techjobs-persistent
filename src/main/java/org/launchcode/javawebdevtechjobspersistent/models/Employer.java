0e00b3a6e979915786f9a6ceb198d80af52ff7d1
name <email@email.com>
2020-01-27 20:28:52 -0600

    On Update Job Model No. 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,11 @@ public class Employer extends AbstractEntity {
     @NotBlank(message="Location is required.")
     @Size(min=2, max=100, message="Location must be between 2 and 100 characters long.")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
 
     public Employer(){
     }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -3,13 +3,8 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.*;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity{
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
 
     private String employer;
     private String skills;
@@ -25,13 +20,6 @@ public class Job{
 
     // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getEmployer() {
         return employer;
